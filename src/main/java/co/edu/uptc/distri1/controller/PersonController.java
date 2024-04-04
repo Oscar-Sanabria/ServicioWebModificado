f2a9e8c1438f53ad92b0833c42fe2f837696c3ea
Oscar-Sanabria <sanabriaoscarjavier1@gmail.com>
2024-04-04 18:27:48 -0500

    first commit

---
+++ src/main/java/co/edu/uptc/distri1/controller/PersonController.java
@@ -0,0 +1,21 @@
+package co.edu.uptc.distri1.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import co.edu.uptc.distri1.model.Person;
+
+@RestController
+@RequestMapping("/persona")
+
+public class PersonController {
+    @GetMapping("/all")
+    public Person getPersonRandom() {
+        Person person = new Person();
+        person.setName("Oscar");
+        person.setLastname("Sanabria");
+        return person;
+    }
+}
